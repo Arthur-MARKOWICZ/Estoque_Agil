4aef2cefd364968f05650f26e924900869d36fbc
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-08-30 09:16:15 -0300

    configurando a branch

---
+++ Testes/Unitarios/UsuarioService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Estoque_Agil.Testes.Unitarios
+{
+    public class UsuarioService
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
